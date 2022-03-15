34148b4702373e5e1596264db89cea7cfafc806e
Pritam-Chakraborty <71542186+Pritam-Chakraborty@users.noreply.github.com>
2022-03-15 11:39:26 +0530

    Add files via upload

---
+++ removefile.py
@@ -0,0 +1,12 @@
+from importlib.resources import path
+import os
+import shutil
+import time
+def converttime():
+    ctime = os.stat(path).st_ctime
+    return ctime
+converttime()
+os.path.exists(path)
+os.walk(path)
+os.path.join()
+os.stat(time.ctime)
